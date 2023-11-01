10c39b1754630b49d7d588d4f034a393f02adf8c
Wassaaa <allarklein@gmail.com>
2023-11-01 19:31:46 +0200

    putnbr fix

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,31 +6,27 @@
 /*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:39:46 by aklein            #+#    #+#             */
-/*   Updated: 2023/10/28 20:32:24 by aklein           ###   ########.fr       */
+/*   Updated: 2023/11/01 19:31:26 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	abs_val(int n)
-{
-	if (n >= 0)
-		return (n);
-	return (n * -1);
-}
-
 void	ft_putnbr_fd(int n, int fd)
 {
-	static int	iteration = 1;
+	long long int nb;
 
-	if (n < 0)
-		ft_putchar_fd('-', fd);
-	if (n != 0)
+	nb = n;
+	if (nb < 0)
 	{
-		iteration = 0;
-		ft_putnbr_fd(abs_val(n / 10), fd);
-		ft_putchar_fd(abs_val(n % 10) + '0', fd);
+		ft_putchar_fd('-', fd);
+		nb *= -1;
 	}
-	else if (iteration)
-		ft_putchar_fd('0', fd);
+	if (nb > 9)
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putchar_fd(nb % 10 + '0', fd);
+	}
+	else
+		ft_putchar_fd(nb + '0', fd);
 }
